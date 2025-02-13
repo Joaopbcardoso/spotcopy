a4cc8e087b5e1161bbb27122c97e1ba1186c771a
joaopbcardoso <joao_pb_cardoso@estudante.sesisenai.org.br>
2025-02-13 11:15:00 -0300

    logica

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,24 +4,31 @@ import artistsArray from '../assets/database/artists';
 import songsArray from '../assets/database/songs';
 
 
-export const Main = () => {
+export const Main = ({ type }) => {
     return (
-        <div className='main'>
-          <ItemList
-           title="Artistas" 
-           items={10} 
-           itemsArray={ artistsArray }
-           path="artists"
-           idPath="artist"
-           />
 
-          <ItemList
-           title="Músicas"
-           items={20} 
-           itemsArray={ songsArray }
-           path="songs"
-           idPath="song"
-           />
+        <div className='main'>
+            {type === 'artists' || type === undefined ? (
+                <ItemList
+                    title="Artistas" 
+                    items={10} 
+                    itemsArray={ artistsArray }
+                    path="artists"
+                    idPath="artist"
+                />
+            ) : (
+                <></>
+            )}
+            {type === 'songs' || type === undefined ? (
+                <ItemList
+                    title="Músicas"
+                    items={20} 
+                    itemsArray={ songsArray }
+                    path="songs"
+                        idPath="song"
+                    /> 
+                ) : <></>}
+            
         </div>
     );
 };
